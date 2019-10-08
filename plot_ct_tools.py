fixed scaling issue when cropping CT images, added function to automatically find and crop images with multiple cores

plot_ct_tools.py
@@ -66,7 +66,7 @@ def ct_in(filename='',xml_fname=''):
     xml_dic = ct_xml(xml_fname)
     return im, xml_dic
 ###############################################################################
-def ct_crop_rotate(ct_data,thresh_val, plot=False):
+def ct_crop_rotate(ct_data,ct_xml,thresh_val, plot=False):
     """
     crop out "air" in a ct image, and rotate to 90 degrees vertical
     As of 4/22/2019, reading in image as 8 bit to perform thresholding,
@@ -121,10 +121,24 @@ def ct_crop_rotate(ct_data,thresh_val, plot=False):
     croppedH = H if not rotated else W
 
     image = cv2.getRectSubPix(
-        cropped, (int(croppedW*scale), int(croppedH*scale)), (size[0]/2, size[1]/2))
+        cropped, (int(croppedW*scale), int(croppedH*scale)),
+                        (size[0]/2, size[1]/2))
+
+    # Update xml so that new images scale correctly, list of dictionaries
+    xml = ct_xml.copy()
+    xml['physical-width'] = image.shape[1]/xml['pixels-per-CM']
+    xml['physical-height'] = image.shape[0]/xml['pixels-per-CM']
+    xml['pixel-width'] = image.shape[1]
+    xml['scan-lines'] = image.shape[0]
+    xml['physical-top'] = top_depths[i]*100.
 
     if plot is True:
-        fig = plt.figure(figsize=(11,17))
+        root = tkinter.Tk()
+        pix2in = root.winfo_fpixels('1i')
+        screen_width = root.winfo_screenwidth()/pix2in
+        screen_height = root.winfo_screenheight()/pix2in
+        image_h2w = round(ct_xml['physical-height']/ct_xml['physical-width'])
+        fig = plt.figure(figsize=(screen_height/image_h2w, screen_height))
         ax1=plt.subplot(121)
         ax1.imshow(thresh,cmap = matplotlib.cm.gray)
         ax1.add_patch(matplotlib.patches.Polygon(xy=box,edgecolor='red',linewidth=0.5,
@@ -134,7 +148,121 @@ def ct_crop_rotate(ct_data,thresh_val, plot=False):
         ax2.imshow(image,cmap = matplotlib.cm.gray)
         ax2.set_title('Cropped and rotated')
 
-    return image
+    return image, xml
+
+###############################################################################
+def ct_crop_rotate_multiple(ct_data,thresh_val,top_depths,plot=False):
+    """
+    crops out "air" in a ct image, and rotates to 90 degrees vertical for
+    a scan with multiple core segments (2-3 Russian cores, for example)
+
+    As of 4/22/2019, reading in image as 8 bit to perform thresholding,
+    apply rotation and cropping to 32 bit image (cv2 doesn't handle 16 bit well)
+
+    ct_data: ct image object read in by "ct_in"
+    thresh_val: threshold value used to classify pixel intensities in grayscale
+    top_depths: depth of the upper parts of each 'segment' in the scan
+    plot: output plots of image processing steps, default is False
+    """
+    n = np.size(top_depths)
+    image_16bit = ct_data.astype('uint16')
+    image_8bit = (image_16bit/256).astype('uint8')
+    image_32bit = ct_data.astype('uint32')
+    # blur to reduce noise
+    blur = cv2.GaussianBlur(image_8bit,(5,5),0)
+
+    # Find edges of core using thresholding
+    ret, thresh = cv2.threshold(blur, thresh_val, 256, 1)
+    thresh = 255-thresh
+    # Find contours of threshold image
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+    # Find the index of the largest contour
+    areas = np.array([cv2.contourArea(c) for c in contours])
+    max_indices = areas.argsort()[-1*n:][::-1]
+
+    cropped_rotated_images = []
+    cropped_rotated_xml = []
+    boxes = []
+    for i,m in enumerate(max_indices):
+        cnt=contours[m]
+        # Find the rotated rectangle that encloses this contour
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        boxes.append(box)
+        #crop image inside bounding box
+        scale = 1  # cropping margin, 1 == no margin
+        W = rect[1][0]
+        H = rect[1][1]
+
+        Xs = [i[0] for i in box]
+        Ys = [i[1] for i in box]
+        x1 = min(Xs)
+        x2 = max(Xs)
+        y1 = min(Ys)
+        y2 = max(Ys)
+
+        angle = rect[2]
+        rotated = False
+        if angle < -45:
+            angle += 90
+            rotated = True
+
+        center = (int((x1+x2)/2), int((y1+y2)/2))
+        size = (int(scale*(x2-x1)), int(scale*(y2-y1)))
+
+        M = cv2.getRotationMatrix2D((size[0]/2, size[1]/2), angle, 1.0)
+        cropped = cv2.getRectSubPix(np.float32(image_32bit), size, center) # converted to 32 bit
+        cropped = cv2.warpAffine(cropped, M, size)
+
+        croppedW = W if not rotated else H
+        croppedH = H if not rotated else W
+
+        image = cv2.getRectSubPix(
+            cropped, (int(croppedW*scale), int(croppedH*scale)),
+                        (size[0]/2, size[1]/2))
+
+        # Add to list of cropped and rotated images
+        cropped_rotated_images.append(image)
+
+        # Update xml so that new images scale correctly, list of dictionaries
+        xml = ct_xml.copy()
+        xml['physical-width'] = image.shape[1]/xml['pixels-per-CM']
+        xml['physical-height'] = image.shape[0]/xml['pixels-per-CM']
+        xml['pixel-width'] = image.shape[1]
+        xml['scan-lines'] = image.shape[0]
+        xml['physical-top'] = top_depths[i]*100.
+        xml['coreID'] = ct_xml['coreID']+str(" %d-%d cm"
+                                %(xml['physical-top']/100,
+                                xml['physical-top']/100+xml['physical-height']))
+        cropped_rotated_xml.append(xml)
+
+        # Plot steps if plot=True
+        if plot is True:
+            ## Get screen size for figure
+            root = tkinter.Tk()
+            pix2in = root.winfo_fpixels('1i')
+            screen_width = root.winfo_screenwidth()/pix2in
+            screen_height = root.winfo_screenheight()/pix2in
+            image_h2w = round(ct_xml['physical-height']/ct_xml['physical-width'])
+            fig = plt.figure(figsize=(screen_height/image_h2w, screen_height))
+
+            ax1=plt.subplot(131)
+            ax1.imshow(ct_data,cmap = matplotlib.cm.gray)
+            ax1.set_title('original image')
+
+            ax2=plt.subplot(132)
+            ax2.imshow(blur,cmap = matplotlib.cm.gray)
+            ax2.set_title('blur')
+
+            ax3=plt.subplot(133)
+            ax3.imshow(thresh,cmap=matplotlib.cm.gray)
+            for b in boxes:
+                ax3.add_patch(matplotlib.patches.Polygon(xy=b,edgecolor='red',linewidth=1.0,
+                                                    fill=False))
+            ax3.set_title('thresholded and contoured')
+    return cropped_rotated_images, cropped_rotated_xml
 
 ###############################################################################
 def ct_plot(ct_data, ct_xml,vmin=0, vmax=50000):

plot_xrf_tools.py
@@ -184,7 +184,7 @@ def nptsmooth(y, n, inf_nan=True, keep_nans=True):
     return out
 
 ###############################################################################
-def plot_xrf_clr(dict, elements, smooth=5 ):
+def plot_xrf_clr(dict, elements, smooth=5,):
     """
     plot centered log ratios or elemental ratios for elements/element pairs as a
     function of depth.
@@ -200,7 +200,10 @@ def plot_xrf_clr(dict, elements, smooth=5 ):
     fig, ax = plt.subplots(nrows = 1, ncols = n, figsize=(3+n,8.5),
                     sharey=True)
     keep_nans=True # for npointssmooth
+    LinearLocator = matplotlib.ticker.LinearLocator
     for i,e in enumerate(elements):
+        ax[i].xaxis.set_major_locator(LinearLocator(2))
+        ax[i].xaxis.set_major_formatter(FormatStrFormatter('%d'))
         if '/' in e:
             dict = makelogratio(dict,e)
             p = ax[i].plot(dict[e],dict['depth'],color = colormap(norm(i)))
@@ -215,13 +218,27 @@ def plot_xrf_clr(dict, elements, smooth=5 ):
                 x = nptsmooth(dict['clr'][:,dict['elements'].index(e)],
                 smooth, keep_nans=keep_nans)
             ax[i].plot(x, dict['depth'], color=colormap(norm(i)))
+
+
+        ax[i].xaxis.set_ticks_position('bottom')
+
+        if i == n-1: # Far right plot needs depth ticks
+            ax[i].spines['left'].set_color('none')
+            ax[i].yaxis.set_ticks_position('right')
+            loc = matplotlib.ticker.MultipleLocator(base=10.0)
+            ax[i].yaxis.set_major_locator(loc)
+            ax[i].yaxis.set_tick_params(labelright=True)
+            ax[i].set_ylabel('Depth in core (cm)')
+            ax[i].yaxis.set_label_position('right')
+        else: # Plots in middle don't need depth ticks
+            ax[i].yaxis.set_ticks([])
+
         ax[i].set_title(e,color=colormap(norm(i)))
         ax[i].yaxis.grid(color='k',linewidth=0.1)
+        ax[0].invert_yaxis()
 
-    ax[0].invert_yaxis()
-    loc = matplotlib.ticker.MultipleLocator(base=10.0)
-    ax[0].yaxis.set_major_locator(loc)
-    ax[-1].yaxis.set_tick_params(labelright=True)
+    return fig
+
 
 # %% TESTING
 filename="/Volumes/tsudisk/Cascadia/Floras Lake/Floras_XRF/VC22-667-817cm_archive/VC22-667-817cm_archive.out"
@@ -229,5 +246,111 @@ dict = xrf_in(filename)
 
 
 # %% Test CLR plots
-elements = ['Ca/Ti','K/Ti','Si/Ti','Si/Al','Fe']
+elements = ['Ca/Ti','K/Ti','Si/Ti','Si/Al','Fe','P','S']
 plot_xrf_clr(dict,elements=elements,smooth=3)
+# %% Test plotting linescan and CT
+matplotlib.rcParams['pdf.fonttype'] = 42
+
+ct_file = '/Volumes/tsudisk/Cascadia/Floras Lake/Floras_RXCT/selected_orthogonal_views/VC22/FLO18-VC22-665-817cm/FLO18-VC22-665-817cm_XZView_corrected.TIF'
+ct, ct_xml = plot_ct_tools.ct_in(ct_file)
+ct = plot_ct_tools.ct_crop_rotate(ct,thresh_val=80, plot=False)
+
+ls_file = '/Volumes/tsudisk/Cascadia/Floras Lake/Floras_LineScan/VC22/FLO18-VC22_667-817cm/IM001_01.tif'
+im, ls_xml = plot_linescan_tools.linescan_in(ls_file)
+
+## Downscale linescan from 16 bit tif to 8 bit
+import skimage
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    im = skimage.img_as_ubyte(im)
+ratio = 150/7.6 # for a typical, 150 cm x 7.6 cm core
+
+## Figure
+## Get screen size for figure
+root = tkinter.Tk()
+pix2in = root.winfo_fpixels('1i')
+screen_width = root.winfo_screenwidth()/pix2in
+screen_height = root.winfo_screenheight()/pix2in
+fig = plt.figure(figsize=(screen_width,screen_height))
+
+nplots = np.size(elements)+2
+
+# Plot CT
+vmin = 15000
+vmax = 30000
+ax = plt.subplot(1,nplots,1)
+ct_img = plt.imshow(ct, aspect=aspect, extent=(0,ct_xml['physical-width'],
+                ct_xml['physical-height']+ct_xml['physical-top']/100,
+                ct_xml['physical-top']/100),vmin=vmin,vmax=vmax,
+                cmap=matplotlib.cm.CMRmap)
+ax.yaxis.set_major_locator(MultipleLocator(10))
+ax.yaxis.set_minor_locator(MultipleLocator(1))
+ax.set_xlim(0,ct_xml['physical-width'])
+ax.set_ylim(ct_xml['physical-height']+ct_xml['physical-top']/100,
+                    ct_xml['physical-top']/100) ## set equal to the linescan
+ax.get_xaxis().set_visible(False)
+ax.set_anchor('NW')
+
+# Plot LineScan
+ax = plt.subplot(1,nplots,2)
+plt.imshow(im, aspect=aspect, extent=(0,ls_xml['physical-width'],\
+                    ls_xml['physical-top']+ls_xml['physical-height'],\
+                    ls_xml['physical-top']))
+ax.yaxis.set_major_locator(MultipleLocator(10))
+ax.yaxis.set_minor_locator(MultipleLocator(1))
+ax.yaxis.tick_right()
+ax.set_xlim(0,ls_xml['physical-width'])
+ax.set_ylim(ls_xml['physical-height']+ls_xml['physical-top'],
+                    ls_xml['physical-top'])
+ax.get_xaxis().set_visible(False)
+ax.set_anchor('NW')
+for spine in ax.spines.values():
+    spine.set_visible(False)
+
+# Plot XRF
+keep_nans=True # for npointssmooth
+LinearLocator = matplotlib.ticker.LinearLocator
+colormap = plt.cm.tab20
+norm = matplotlib.colors.Normalize(vmin=0,vmax = np.size(elements))
+n = np.size(elements)
+smooth=3
+depth = ls_xml['physical-top'] + dict['section depth']
+for i,e in enumerate(elements):
+    ax = plt.subplot(1, nplots, i+3)
+    ax.xaxis.set_major_locator(LinearLocator(2))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.set_ylim(np.max(depth),np.min(depth))
+    if '/' in e:
+        dict = makelogratio(dict,e)
+        p = ax.plot(dict[e],depth,color = colormap(norm(i)))
+    else:
+        clr_vector = dict['clr'][:,dict['elements'].index(e)]
+        p = ax.plot(clr_vector,depth,color = colormap(norm(i)))
+    if smooth:
+        p[0].set_alpha(0.4)
+        if '/' in e:
+            x = nptsmooth(dict[e], smooth, keep_nans=keep_nans)
+        else:
+            x = nptsmooth(dict['clr'][:,dict['elements'].index(e)],
+            smooth, keep_nans=keep_nans)
+        ax.plot(x, depth, color=colormap(norm(i)))
+
+
+    ax.xaxis.set_ticks_position('bottom')
+
+    if i == n-1: # Far right plot needs depth ticks
+        ax.spines['left'].set_color('none')
+        ax.yaxis.set_ticks_position('right')
+        loc = matplotlib.ticker.MultipleLocator(base=10.0)
+        ax.yaxis.set_major_locator(loc)
+        ax.yaxis.set_tick_params(labelright=True)
+        ax.set_ylabel('Depth in core (cm)')
+        ax.yaxis.set_label_position('right')
+    else: # Plots in middle don't need depth ticks
+        ax.yaxis.set_ticks([])
+
+    ax.set_title(e,color=colormap(norm(i)))
+    ax.yaxis.grid(color='k',linewidth=0.1)
+
+plt.savefig('test.pdf',format='pdf',bbox_inches='tight')
+    