1cf4a5b0d2c104cd5b412e99b7d56d94ae7062c2
slaselle <slaselle@usgs.gov>
2021-10-28 14:42:32 -0700

    created pypi package for pip installing corescan_plotting

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages, Extension
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.8'
+DESCRIPTION = 'Python tools for plotting scanned core data from the USGS Pacific Coastal and Marine Science Center'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="corescan_plotting",
+    version=VERSION,
+    author="SeanPaul La Selle",
+    author_email="<slaselle@usgs.gov>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'geology', 'core', 'ct', 'xrf', 'linescan'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
